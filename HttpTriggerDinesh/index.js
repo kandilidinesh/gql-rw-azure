dc4240ff9fc5537fe0668772e03cd488e4d3803d
kandilidinesh <kandilidinesh@gmail.com>
2020-07-14 16:51:17 +0530

    Final Commit. Azure Functions Working.

--- HttpTriggerDinesh/index.js
+++ HttpTriggerDinesh/index.js
@@ -1,23 +1,16 @@
-// const express = require('express');
-// const {ApolloServer, gql} = require('apollo-server-express');
-import { ApolloServer, ApolloError } from "apollo-server-azure-functions";
+const { ApolloServer, ApolloError }= require("apollo-server-azure-functions");
 const cors = require('cors');
 require('dotenv').config();
 const {v4} = require('uuid');
 
-const pool = require("./db");
-const schema = require('./schema/index');
-const resolvers = require('./resolvers/index');
-const models = require('./models/index');
+const pool = require("./src/db");
+const schema = require('./src/schema/index');
+const resolvers = require('./src/resolvers/index');
+const models = require('./src/models/index');
 
 const messages = models.messages;
 const users = models.users;
 
-// const app = express();
-
-// app.use(cors());
-
-// const server = new ApolloServer({typeDefs:schema, resolvers, context: {me: users[1]}});
 const server = new ApolloServer({typeDefs:schema, resolvers, context:({request}) => ({
     request,
     pool,
@@ -27,14 +20,9 @@ const server = new ApolloServer({typeDefs:schema, resolvers, context:({request})
     v4
 })});
 
-export default server.createHandler({
+module.exports = server.createHandler({
     cors: {
         origin: '*',
         credentials: true,
     },
 });
-// server.applyMiddleware({ app, path: '/graphql' });
-
-// app.listen({port: 8000}, () => {
-//     console.log(`Apollo Server is running on port http://localhost:8000/graphql`);
-// });
